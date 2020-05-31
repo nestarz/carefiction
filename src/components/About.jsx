8ae74059f179308b5ac764bf4edc1bdf7050e3f9
Elias Rhouzlane <eliasrhouzlane@Eliass-MacBook-Pro.local>
2020-05-31 21:48:57 +0200

    fix: stykle

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,20 +9,29 @@ export default ({ node }) => {
         <span>CARE FICTION:</span>
         <span>A PLAYGROUND FOR COMMONERS</span>
       </h1>
-      <h2>Welcome!</h2>
       <p>
-        <i>Care Fiction</i> is the prototype of a playground for collaborative
-        dreaming, envisioning, conceiving, projecting. It all starts with a
-        targeted resource, or commons. We invite our guests to react to an
-        existing value, by pouring their ideas, resources, scenarios, inside the
-        container.
+        Welcome! You're here because we would like you to join us in a game of
+        collaborative fiction writing around and about a commoning project.
+        "Commoning" is an ombrella term that brings together all the different
+        ways people achieve to share the access and care of material and
+        immaterial resources that in our society are more likely to be
+        distributed top-down. Anything can be a common. A oven in a small
+        village, an open source archive, a vegetables garden.
       </p>
       <p>
-        Our generative fiction tool will combine all contributions and produce
-        an ever changing story. The more we all add, the more the story becomes
-        richer, and selected. May the dream breach through the screen and become
-        real.
+        We started fiction 0, "The White House", and we'd like to to imagine
+        with us how a scenario of commoning could look like. We put a few
+        rudimental tools in place for you to co-write the story, to dream,
+        envision, conceive, project. The more we all add, the more the story
+        becomes richer, and selected. May the dream breach through the screen
+        and become real.
       </p>
+      <p>
+        We are three students from Design Academy Eindhoven who are interested
+        in the topic of commoning. Feel free to get more involved, send us an
+        email.
+      </p>
+
       <Fictions node={node} />
     </div>
   );
